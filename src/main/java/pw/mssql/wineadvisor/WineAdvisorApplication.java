ac9daa3ddb9d50602f58d09c01c994081c373cca
WoKak <michal.woj34@gmail.com>
2018-04-19 10:34:22 +0200

    initial commit

---
+++ src/main/java/pw/mssql/wineadvisor/WineAdvisorApplication.java
@@ -0,0 +1,12 @@
+package pw.mssql.wineadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineAdvisorApplication.class, args);
+    }
+}
